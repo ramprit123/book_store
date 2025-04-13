be395253f9e5913f2b8d979eb013ac54086b6b6b
Ramprit Sahani <ramprit.sahani@outlook.com>
2025-04-13 17:22:51 +0530

    Auth form UI added

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,7 @@
 import { Ionicons } from '@expo/vector-icons'
-import { Text, View } from 'react-native'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import { KeyboardAvoidingView, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Button } from '~/components/Button'
 import Image from '~/components/Image'
@@ -8,24 +10,50 @@ import PasswordInput from '~/components/PasswordInput'
 import COLORS from '~/constants/colors'
 
 const SignIn = () => {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSignIn = () => {
+    // Handle sign in logic here
+    console.log('Email:', email)
+    console.log('Password:', password)
+  }
   return (
     <SafeAreaView className='flex-1 bg-background'>
-      <View className='flex-1 items-center justify-center mx-6 gap-4'>
-        <Image
-          source={require('~/assets/img/i.png')}
-          width={200}
-          height={200}
-          resizeMode='contain'
-        />
-        <View className='w-full p-10 bg-cardBackground gap-6 rounded-lg'>
-          <Input keyboardType='email-address' placeholder='Email or Username' leftIcon={<Ionicons name="mail-outline" size={24} color={COLORS.primary} />} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className='flex-1'
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View className='flex-1 items-center justify-center mx-6 gap-4'>
+            <Image
+              source={require('~/assets/img/i.png')}
+              width={200}
+              height={200}
+            />
+            <View className='w-full p-6 bg-cardBackground gap-6 rounded-lg'>
+              <Input keyboardType='email-address' placeholder='Email or Username' leftIcon={<Ionicons name="mail-outline" size={24} color={COLORS.primary} />} />
 
-          <PasswordInput placeholder='Password' />
-          <View className='w-full mt-6'>
-            <Button title='Sign In' />
+              <PasswordInput placeholder='Password' />
+              <View className='w-full mt-4'>
+                <Button title='Sign In' />
+                <View className='flex-row items-center justify-center mt-4'>
+                  <Text className=' text-textDark'>Do not have an account?</Text>
+                  <TouchableOpacity className='ml-1' onPress={() => router.push("/(auth)/signup")}>
+                    <Text className='text-primary font-semibold'>Sign Up</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
           </View>
-        </View>
-      </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,10 +1,63 @@
-import { Text, View } from 'react-native'
+import { Feather, Ionicons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import { KeyboardAvoidingView, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Button } from '~/components/Button'
+import Image from '~/components/Image'
+import Input from '~/components/Input'
+import PasswordInput from '~/components/PasswordInput'
+import COLORS from '~/constants/colors'
 
 const SignUp = () => {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSignIn = () => {
+    // Handle sign in logic here
+    console.log('Name:', name)
+    console.log('Email:', email)
+    console.log('Password:', password)
+  }
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-background'>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className='flex-1'
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View className='flex-1 items-center justify-center mx-6 gap-4'>
+            <Image
+              source={require('~/assets/img/i.png')}
+              width={200}
+              height={200}
+            />
+            <View className='w-full p-6 bg-cardBackground gap-6 rounded-lg'>
+              <Input placeholder='Full name' leftIcon={<Feather name="user" size={24} color={COLORS.primary} />} />
+              <Input keyboardType='email-address' placeholder='Email or Username' leftIcon={<Ionicons name="mail-outline" size={24} color={COLORS.primary} />} />
+
+              <PasswordInput placeholder='Password' />
+              <View className='w-full mt-4'>
+                <Button title='Sign Up' />
+                <View className='flex-row items-center justify-center mt-4'>
+                  <Text className=' text-textDark'>Already have an account?</Text>
+                  <TouchableOpacity className='ml-1' onPress={() => router.push("/(auth)")}>
+                    <Text className='text-primary font-semibold'>Sign In</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
